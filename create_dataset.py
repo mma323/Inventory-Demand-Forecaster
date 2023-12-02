8acfc9478b5757f8f5b798fddbe3edda5f77f4e5
Mikael Johansen Mahlio <mma323@uit.no>
2023-12-02 19:06:12 +0100

    Added tests to create_dataset and decision_tree_regressor

--- create_dataset.py
+++ create_dataset.py
@@ -1,39 +1,114 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-def create_dataset(csv_path : str, column : str, window_size : int) -> tuple:
+
+def plot_data(csv_path : str, data_column : str, time_column : str) -> None:
     """
-        Reads data from a csv file and creates a dataset using the
-        sliding window method.
+        Plots the data from a csv file to see what it looks like in order to 
+        determine window size for sliding window.
 
         Parameters
         ----------
         csv_path : str
-            Path to the csv file.
-        column : str
-            Name of the column in the csv file that will be used to create 
-            the dataset.
-        window_size : int
-            Size of the window that will be used to create the dataset in
-            the sliding window method.
+            Path to the csv file containing the data.
+        data_column : str
+            Name of the column in the csv file that contains the data.
+        time_column : str
+            Name of the column in the csv file that contains the time values.
+        """
+    data_frame = pd.read_csv(csv_path)
+    data = data_frame[data_column].values
+    time = data_frame[time_column].values
+
+    plt.plot(time, data)
+    plt.show()
+
+
+def split(data : pd.DataFrame, training_fraction : float) -> tuple:
+    """
+        Splits the data into training and testing data.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Data to be split.
+        training_fraction : float
+            Fraction of the data that should be used for training.
 
         Returns
         -------
-        data_points : list of lists
-            List of lists containing the data points that are used as input
-        expected_outputs : list
-            List containing the next data point after the data points in
-            each list of data_points.
+        training_data : pd.DataFrame
+            Training data.
+        testing_data : pd.DataFrame
+            Testing data.
         """
-    data_frame = pd.read_csv(csv_path)
-    data = data_frame[column].values
+    split_index = int(len(data) * training_fraction)
+    training_data = data[:split_index]
+    testing_data = data[split_index:]
 
-    data_points, expected_outputs = [], []
+    return training_data, testing_data
+
+
+def create_dataset(
+        data : pd.DataFrame, 
+        time_column : str, 
+        data_column : str, 
+        window_size : int
+) -> tuple:
+    """
+        Creates a dataset from the data provided using a sliding window.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Data to create the dataset from.
+        time_column : str
+            Name of the column in the csv file that contains the time values.
+        data_column : str
+            Name of the column in the csv file that contains the data.
+        window_size : int
+            Size of the sliding window.
+
+        Returns
+        -------
+        dataset : tuple
+            Tuple containing the data points, expected outputs and time values.
+            data_points : list of lists
+                List of lists containing the data points that are used as input
+            expected_outputs : list
+                List containing the next data point after the data points in
+                each list of data_points.
+            time : list
+                List containing the time values for each data point.
+        """
+    data_points = []
+    expected_outputs = []
+    time = []
 
     for i in range(len(data) - window_size):
-        data_points.append( data[i : i + window_size] )
-        expected_outputs.append( data[i + window_size] )
+        data_points.append(data[data_column][i:i + window_size].values)
+        expected_outputs.append(data[data_column][i + window_size])
+        time.append(data[time_column][i + window_size])
+
+    return data_points, expected_outputs, time
+
+
+def main():
+    #For testing purposes
+    file = "data_2022.csv"
+    data_column = "Demand"
+    time_column = "Date"
+    window_size = 30
+
+    plot_data(file, data_column, time_column)
+
+    data_frame = pd.read_csv(file)
+    training_data, testing_data = split(data_frame, 0.8)
+    training_dataset = create_dataset(training_data, time_column, data_column, window_size)
     
-    return data_points, expected_outputs
+    print(training_dataset)
 
 
 
+if __name__ == "__main__":
+    main()
---
+++ decision_tree_regressor.py
@@ -0,0 +1,62 @@
+from create_dataset import create_dataset, split
+from sklearn.tree import DecisionTreeRegressor
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def train_regression_tree(dataset : tuple):
+    """
+        Trains a regression tree using the dataset provided.
+
+        Parameters
+        ----------
+        dataset : tuple
+            Tuple containing the data points, expected outputs and time values.
+            data_points : list of lists
+                List of lists containing the data points that are used as input
+            expected_outputs : list
+                List containing the next data point after the data points in
+                each list of data_points.
+            time : list
+                List containing the time values for each data point, is not
+                used in this function.
+
+        Returns
+        -------
+        regressor : DecisionTreeRegressor
+            Trained regression tree.
+        """
+    data_points, expected_outputs, time = dataset
+    regressor = DecisionTreeRegressor(random_state=0)
+    regressor.fit(data_points, expected_outputs)
+
+    return regressor
+
+
+def main():
+    window_size = 5
+    data = pd.read_csv("data_2022.csv")
+    training_data, testing_data = split(data, 0.8)
+    training_dataset = create_dataset(training_data, "Date", "Demand", window_size)
+    regressor = train_regression_tree(training_dataset)
+
+    
+    #Using the same sliding window approach as in (2.a), start from 01.01.2022 and use the
+    #regression tree to predict the next value. Save each prediction in a list.
+    
+    predictions = []
+    for i in range(len(testing_data) - window_size):
+        data_points = testing_data["Demand"].iloc[i:i + window_size].values
+        predictions.append(regressor.predict([data_points]))
+
+
+    #Use matplotlib to plot the results alongside the original data.
+    #you have to offset the predictions by 30 days, since you are predicting the next value
+    #after the sliding window.
+    plt.plot(testing_data["Date"].iloc[window_size:], predictions, label="Predictions")
+    plt.plot(testing_data["Date"].iloc[window_size:], testing_data["Demand"].iloc[window_size:], label="Original data")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
