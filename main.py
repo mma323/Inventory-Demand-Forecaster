695ab804929b233944040d8621933edf4c9940ad
Mikael Johansen Mahlio <mma323@uit.no>
2023-12-02 23:09:27 +0100

    Now loading in data_2023.csv and creating a dataset from it

--- main.py
+++ main.py
@@ -3,5 +3,25 @@ import decision_tree_regressor
 import random_forest
 import MLPRegressors
 import matplotlib.pyplot as plt
+import pandas as pd
 
-plt.show()
+
+def main():
+    plt.show()
+
+    window_size = 5
+    data = pd.read_csv("data_2023.csv")
+    training_data, testing_data = create_dataset.split(data, 0.8)
+
+    training_dataset = create_dataset.create_dataset(
+        training_data, "Date", "Demand", window_size
+    )
+    testing_dataset = create_dataset.create_dataset(
+        testing_data, "Date", "Demand", window_size
+    )
+
+
+if __name__ == "__main__":
+    main()
+
+
